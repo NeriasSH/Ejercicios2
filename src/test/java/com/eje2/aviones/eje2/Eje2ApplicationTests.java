83b5682d65e79fa2810e0b1d0bccab640e7c4a0f
NeriasSH <nesh.nerias@gmail.com>
2024-11-16 14:06:38 -0500

    first commit

---
+++ src/test/java/com/eje2/aviones/eje2/Eje2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eje2.aviones.eje2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eje2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
